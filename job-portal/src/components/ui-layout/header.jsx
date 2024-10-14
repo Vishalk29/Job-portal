5ce64955a209377a1f7938c78c1786b7dddcc7ff
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2024-10-14 22:47:34 +0530

    added the one more button post job that will show once we login

--- job-portal/src/components/ui-layout/header.jsx
+++ job-portal/src/components/ui-layout/header.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import { PenBox } from "lucide-react";
 
 const Header = () => {
   return (
@@ -14,13 +10,21 @@ const Header = () => {
       <Link>
         <img src="/public/logo.png" alt="Hired logo" className="h-20" />
       </Link>
-      <Button variant="outline">Login</Button>
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
+      <div className="flex gap-8">
+        <SignedOut>
+          <Button variant="outline">Login</Button>
+        </SignedOut>
+        <SignedIn>
+          <Link to="/post-job">
+            {/* Add condition our here */}
+            <Button variant="destructive" className="rounded-full">
+              <PenBox size={20} className="mr-2" />
+              Post a Jobs
+            </Button>
+          </Link>
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
   );
 };

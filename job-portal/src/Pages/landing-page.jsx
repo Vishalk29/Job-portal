520a6401d29b070e59db0eb0d0ddf1ec6c775d71
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2024-10-14 13:19:37 +0530

    added the heading text in landing page and passed the color for that and tailwind css property

--- job-portal/src/Pages/landing-page.jsx
+++ job-portal/src/Pages/landing-page.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center">
+        <h1 className="flex flex-col justify-center items-center gradient-title text-4xl sm:text-6xl lg:text-8xl tracking-tighter font-extrabold py-4">
+          Find Your Dream Job{" "}
+          <span className="flex items-center gap-5 sm:gap-6">
+            and get{" "}
+            <img
+              src="/public/logo.png"
+              alt="hired logo"
+              className="h-14 sm:h-24 lg:h-32"
+            />
+          </span>
+        </h1>
+      </section>
+      <div>
+        {/* Button */}
+        {/* Carosusal */}
+      </div>
+      {/* banner */}
+      <section>{/* card */}</section>
+      {/* Accordian */}
+    </main>
+  );
 };
 
 export default LandingPage;

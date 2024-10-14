1e3cf7e80adf1289db84b7d929be609a1f6f6599
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2024-10-14 18:21:08 +0530

    added the Carousel to landing page and banner also

--- job-portal/src/Pages/landing-page.jsx
+++ job-portal/src/Pages/landing-page.jsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import companies from "../data/companies.json";
 const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
@@ -21,23 +27,42 @@ const LandingPage = () => {
           Explore thousands of job listing or find the perfect candidate
         </p>
       </section>
-      <div>
-        {/* Button */}
-        <div className="flex gap-6 justify-center">
-          <Link to="/jobs">
-            <Button variant="blue" size="xl">
-              Find Jobs
-            </Button>
-          </Link>
-          <Link to="/post-job">
-            <Button variant="destructive" size="xl">
-              Post a Jobs
-            </Button>
-          </Link>
-        </div>
-        {/* Carosusal */}
+      {/* Button */}
+      <div className="flex gap-6 justify-center">
+        <Link to="/jobs">
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to="/post-job">
+          <Button variant="destructive" size="xl">
+            Post a Jobs
+          </Button>
+        </Link>
       </div>
+      {/* Carosusal */}
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
       {/* banner */}
+      <img src="/public/banner.jpeg" alt="banner image" className="w-full" />
       <section>{/* card */}</section>
       {/* Accordian */}
     </main>

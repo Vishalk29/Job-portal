726bbe67ed45f50797c0bf4dfeaf88a571df74e5
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2024-10-15 23:32:07 +0530

    used the bar loader from react-spinner

--- job-portal/src/Pages/onboardingPage.jsx
+++ job-portal/src/Pages/onboardingPage.jsx
@@ -1,7 +1,17 @@
+import { useUser } from "@clerk/clerk-react";
 import React from "react";
+import { BarLoader, ClockLoader } from "react-spinners";
 
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return (
+    <div>
+      <h1 className="text-white">Helllpop</h1>
+    </div>
+  );
 };
 
 export default OnboardingPage;

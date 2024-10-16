7cf5f6026e56b756d00a406fc65df17aed5d8931
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2024-10-17 00:10:43 +0530

    added the search functionality in code

--- job-portal/src/Pages/job-listing.jsx
+++ job-portal/src/Pages/job-listing.jsx
@@ -1,5 +1,8 @@
+import { getCompanies } from "@/api/apiCompanies";
 import { getJobs } from "@/api/apiJobs";
 import JobCard from "@/components/ui-layout/job-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useFetch } from "@/hooks/use-fetch";
 import { useUser } from "@clerk/clerk-react";
 import React, { useEffect, useState } from "react";
@@ -17,9 +20,26 @@ export const JobListing = () => {
     loading: loadingJobs,
     error: errorJobs,
   } = useFetch(getJobs, { location, searchQuery, company_id });
+
   useEffect(() => {
     if (isLoaded) fetchfnJobs();
   }, [isLoaded, location, searchQuery, company_id]);
+
+  const {
+    fetchfn: fetchfnCompanies,
+    data: dataCompanies,
+    error: errorCompanies,
+  } = useFetch(getCompanies);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+  useEffect(() => {
+    if (isLoaded) fetchfnCompanies();
+  }, [isLoaded]);
   // This loader for user loadin
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
@@ -30,6 +50,21 @@ export const JobListing = () => {
         Latest Job
       </h1>
       {/* Add filters Here */}
+      <form
+        className="h-14 flex flex-row gap-2 items-center mb-3"
+        onSubmit={handleSearch}
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs by Title.."
+          name="search-query"
+          className="h-full px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
+
       {/* This loader for jobs loading */}
       {loadingJobs && (
         <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
